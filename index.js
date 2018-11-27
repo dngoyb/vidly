ea399384c78d35707b1a8caec7e98e53c502ed6f
dngoyb <dngoyb@gmail.com>
2018-11-27 12:34:39 +0200

    adding movie routes and models

--- index.js
+++ index.js
@@ -2,8 +2,10 @@
 const mongoose = require('mongoose')
 const Joi = require('joi');
 const express = require('express');
-const genres = require('./routes/genres')
-const customers = require('./routes/customers')
+const genres = require('./routes/genres');
+const customers = require('./routes/customers');
+const movies = require('./routes/movies');
+
 const app = express();
 
 mongoose.connect('mongodb://localhost/vidly')
@@ -15,6 +17,7 @@ mongoose.connect('mongodb://localhost/vidly')
 app.use(express.json());
 app.use('/api/genres', genres)
 app.use('/api/customers', customers)
+app.use('/api/movies', movies)
 
 app.get('/', (req, res) => {
     res.send('<h1>Hello World</h1>')
---
+++ models/movies.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose');
+const Joi = require('joi');
+const {genreSchema} = require('./genres');
+
+const Genre = mongoose.model('Movie', new mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+        trim: true, 
+        minlength: 5,
+        maxlength: 255
+      },
+      genre: { 
+        type: genreSchema,  
+        required: true
+      },
+      numberInStock: { 
+        type: Number, 
+        required: true,
+        min: 0,
+        max: 255
+      },
+      dailyRentalRate: { 
+        type: Number, 
+        required: true,
+        min: 0,
+        max: 255
+      }
+  }));
+
+  function validateMovie(movie) {
+    const schema = {
+      title: Joi.string().min(3).required(),
+      genre: Joi.string().required(),
+      numberInStock: Joi.number().min(0).required(),
+      dailyRentalRate: Joi.number().min(0).required()
+    };
+  
+    return Joi.validate(movie, schema);
+  }
+
+  exports.Movie = Movie
+  exports.validateMovie = validateMovie
